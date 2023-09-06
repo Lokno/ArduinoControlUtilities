af37b743715468f27672a9808a9687036666ac0f
Jonathan Decker <lokno1@gmail.com>
2023-09-06 13:17:00 -0400

    Update install_python_dependencies.bat
    Added dependencies for the servo_sketch_generator.py script

--- install_python_dependencies.bat
+++ install_python_dependencies.bat
@@ -1,10 +1,35 @@
 @echo off
-echo Installing pyfirmata and tkdial packages via pip...
-pip install pyfirmata tkdial
+
+echo Installing pyfirmata package via pip...
+pip install pyfirmata
 if %errorlevel% neq 0 (
     echo An error occurred during installation.
-    pause
 ) else (
     echo Installation completed successfully.
-    pause
 )
+
+echo Installing tkdial package via pip...
+pip install tkdial
+if %errorlevel% neq 0 (
+    echo An error occurred during installation.
+) else (
+    echo Installation completed successfully.
+)
+
+echo Installing python-benedict package via pip...
+pip install python-benedict
+if %errorlevel% neq 0 (
+    echo An error occurred during installation.
+) else (
+    echo Installation completed successfully. 
+)
+
+echo Installing appdirs package via pip...
+pip install appdirs
+if %errorlevel% neq 0 (
+    echo An error occurred during installation.
+) else (
+    echo Installation completed successfully. 
+)
+
+pause
