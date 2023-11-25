f2013f6a3842e2fb208e8b311532fdc8873c724c
Lokno Ketchup <lokno1@gmail.com>
2023-11-24 23:03:59 -0500

    Added a GUI to websocket_pyfirmata.py if no cmdline arguments are given

--- websocket_pyfirmata.py
+++ websocket_pyfirmata.py
@@ -9,6 +9,7 @@ import pyfirmata
 import json
 import platform
 import argparse
+import logging
 
 major,minor,_ = platform.python_version_tuple()
 if major != '3':
@@ -19,30 +20,6 @@ if minor >= '11':
     import inspect
     if not hasattr(inspect, 'getargspec'):
         inspect.getargspec = inspect.getfullargspec
-
-def write_csv_row(filename, data): 
-
-    d = zip(data.keys(),data.values())
-    d = sorted(d)
-    keys,values = zip(*d)
-
-    if int(data["frame"]) == 1:
-        with open(filename,'w') as f:
-            f.write(','.join(keys) + '\n')
-
-    with open(filename,'a') as f:
-        l = ''
-        for i,v in enumerate(values):
-            if isinstance(v,str):
-                l += v
-            elif isinstance(v,float):
-                l += str(int(v))
-            else:
-                l += str(v)
-            if i < (len(values) - 1):
-                l += ','
-        f.write(l + '\n')
-
 
 class BoardControl:
     def __init__(self):
@@ -98,45 +75,277 @@ class BoardControl:
                 self.io[pin].write(self.clamp(value,0.0,1.0))
             elif io_type == 'analog_pwm':
                 self.io[pin].write(self.clamp(value,0.0,1.0))
-        
 
-connected_clients = set()
-board_ctrl = BoardControl()
-write_csv = False
-serve_host = "127.0.0.1"
-serve_port = 22300
-csv_file_name = "output.csv"
+class WebSocketServer:
+    def __init__(self):
+        self.host = "127.0.0.1"
+        self.port = 22300
+        self.server = None
+        self.connected_clients = set()
+        self.board_ctrl = BoardControl()
+        self.write_csv = False
+        self.csv_file_name = None
 
-async def register(websocket):
-    connected_clients.add(websocket)
-    print(f"Client connected: {websocket.remote_address}")
+    async def register(self,websocket):
+        self.connected_clients.add(websocket)
+        print(f"Client connected: {websocket.remote_address}")
+    
+    async def unregister(self,websocket):
+        self.connected_clients.remove(websocket)
+        print(f"Client disconnected: {websocket.remote_address}")
 
-async def unregister(websocket):
-    connected_clients.remove(websocket)
-    print(f"Client disconnected: {websocket.remote_address}")
+    async def echo(self, websocket, path):
+        await self.register(websocket)
+        try:
+            async for message in websocket:
+                logging.debug(message)
+                data = json.loads(message)
+                self.board_ctrl.update(data['port'],data)
 
-async def echo(websocket, path):
-    await register(websocket)
-    try:
-        async for message in websocket:
-            data = json.loads(message)
-            board_ctrl.update(data['port'],data)
+                if self.write_csv:
+                    self.write_csv_row(self.csv_file_name,data)
+        except websockets.exceptions.ConnectionClosed as e:
+            print(f"Connection closed with {websocket.remote_address}: {e.reason}")
+        finally:
+            await self.unregister(websocket)
 
-            if write_csv:
-                write_csv_row(csv_file_name,data)
+    async def show(self):
+        while self.showing:
+            self.root.update()
+            await asyncio.sleep(0.1)
 
-    except websockets.exceptions.ConnectionClosed as e:
-        print(f"Connection closed with {websocket.remote_address}: {e.reason}")
-    finally:
-        await unregister(websocket)
+    async def main(self):
+        try:
+            self.server = await websockets.serve(self.echo, self.host, self.port)
+            await self.server.wait_closed()
+        except asyncio.exceptions.CancelledError as e:
+            pass
 
-async def main():
+    def set_host(self, host):
+        self.host = host
 
-    async with websockets.serve(echo, serve_host, serve_port):
-        print(f'Serving Websocket at {serve_host}:{serve_port}')
-        if write_csv:
-            print(f'Writing output to {csv_file_name}')
-        await asyncio.Future()
+    def set_port(self, port):
+        self.port = port
+
+    def set_csv_file(self, csv_file_name):
+        self.csv_file_name = csv_file_name
+
+    def enable_csv(self, enable):
+        self.write_csv = enable
+
+    def start(self):
+        if not self.is_running():
+            asyncio.create_task(self.main())
+
+    def stop(self):
+        if self.is_running():
+            self.server.close()
+
+    def is_running(self):
+        return self.server is not None and self.server.is_serving()
+
+    def write_csv_row(self, filename, data): 
+        d = zip(data.keys(),data.values())
+        d = sorted(d)
+        keys,values = zip(*d)
+    
+        if not os.path.exists(self.csv_file_name):
+            with open(filename,'w') as f:
+                f.write(','.join(keys) + '\n')
+    
+        with open(filename,'a') as f:
+            l = ''
+            for i,v in enumerate(values):
+                if isinstance(v,str):
+                    l += v
+                elif isinstance(v,float):
+                    l += str(int(v))
+                else:
+                    l += str(v)
+                if i < (len(values) - 1):
+                    l += ','
+            f.write(l + '\n')
+
+    def clear_csv(self):
+        if self.csv_file_name is not None and os.path.exists(self.csv_file_name):
+            os.remove(self.csv_file_name)
+
+class WebSocketGUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Websocket PyFirmata")
+
+        self.app_name = "WebsocketPyFirmata"
+        self.app_author = "Lokno"
+
+        config_data = self.load_config()
+
+        if not config_data:
+            config_data['csv_file'] = ''
+            config_data['host'] = "127.0.0.1"
+            config_data['port'] = '22300'
+
+        # Host and Port Entry
+        self.server_frame = tk.Frame(self.root)
+        self.server_frame.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+
+        self.host_label = tk.Label(self.server_frame, text="Host:")
+        self.host_label.pack(side="left")
+        self.host_entry = tk.Entry(self.server_frame)
+        self.host_entry.pack(side="left", padx=5)
+        self.host_entry.insert("end",config_data['host'])  
+
+        self.port_label = tk.Label(self.server_frame, text="Port:")
+        self.port_label.pack(side="left", padx=5)
+        self.port_entry = tk.Entry(self.server_frame, width=10)
+        self.port_entry.pack(side="left", padx=5)
+        self.port_entry.insert("end",config_data['port'])  
+
+        # Run and Stop Buttons
+        self.run_button = tk.Button(self.server_frame, text="Run", command=self.start_server)
+        self.run_button.pack(side="left", padx=5)
+
+        self.stop_button = tk.Button(self.server_frame, text="Stop", command=self.stop_server)
+        self.stop_button.pack(side="left", padx=5)
+        self.stop_button["state"] = tk.DISABLED
+
+        # CSV File Entry and Browse Button
+        self.csv_frame = tk.Frame(self.root)
+        self.csv_frame.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        self.csv_label = tk.Label(self.csv_frame, text="CSV File:")
+        self.csv_label.pack(side="left")
+        self.csv_entry = tk.Entry(self.csv_frame)
+        self.csv_entry.pack(side="left", padx=5)
+        self.csv_entry.insert("end",config_data['csv_file'])  
+
+        self.browse_button = tk.Button(self.csv_frame, text="Browse", command=self.browse)
+        self.browse_button.pack(side="left", padx=5)
+
+        # Enable CSV Output Checkbox and Clear CSV Button
+        self.enable_csv_var = tk.IntVar()
+        self.enable_csv_var.trace_add("write", self.checkbox_changed) 
+        self.enable_csv_checkbox = tk.Checkbutton(self.csv_frame, text="Enable CSV Output", variable=self.enable_csv_var)
+        self.enable_csv_checkbox.pack(side="left", padx=5)
+
+        self.clear_csv_button = tk.Button(self.csv_frame, text="Clear CSV", command=self.clear_csv)
+        self.clear_csv_button.pack(side="left", padx=5)
+
+        self.message_text = tk.Label(self.root, height=1, width=40)
+        self.message_text.grid(row=2, column=0, columnspan=2, pady=5)
+
+        self.server = WebSocketServer()
+        self.server.set_host(config_data['host'])
+        self.server.set_port(config_data['port'])
+        self.server.set_csv_file(config_data['csv_file'])
+        self.server.enable_csv(False)
+
+        self.event_loop = asyncio.get_event_loop()
+
+        self.showing = True
+
+        self.re_host = re.compile("([0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+|localhost)")
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def update_status(self, message, is_error=False):
+        self.message_text.config(text=message)
+        if is_error:
+            self.message_text.config(foreground="red")
+        else:
+            self.message_text.config(foreground="black")
+
+    def load_config(self):
+        data_dir = appdirs.user_data_dir(self.app_name, self.app_author)
+        data_file = os.path.join(data_dir, "config.json")
+        if not os.path.exists(data_file):
+            return {}
+        with open(data_file, "r") as f:
+            return json.loads(f.read())
+
+    def store_config(self,data):
+        data_dir = appdirs.user_data_dir(self.app_name, self.app_author)
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        data_file = os.path.join(data_dir, "config.json")
+        with open(data_file, "w") as f:
+            f.write(json.dumps(data))
+
+    async def show(self):
+        while self.showing:
+            self.root.update()
+            await asyncio.sleep(0.1)
+
+    def start_server(self):
+        if not self.server.is_running():
+
+            host = self.host_entry.get()
+            port = self.port_entry.get()
+
+            if not self.re_host.match(host):
+                self.update_status('Host name not valid',True)
+            elif not port.isnumeric():
+                self.update_status('Port not valid',True)
+            else:
+
+                self.server.set_host(host)
+                self.server.set_port(port)
+                self.update_status('Running')
+                self.server.start();
+                self.run_button["state"] = tk.DISABLED
+                self.stop_button["state"] = tk.NORMAL
+        else:
+            self.update_status('Server is running',True)
+
+    def stop_server(self):
+        if self.server.is_running():
+            self.server.stop()
+            self.update_status('Stopping')
+            self.run_button["state"] = tk.NORMAL
+            self.stop_button["state"] = tk.DISABLED
+        else:
+            self.update_status('Server not running',True)
+
+    def checkbox_changed(self, *args):
+        if self.enable_csv_var.get() == 1:
+            logging.debug("CSV Output Enabled")
+            self.server.enable_csv(True)
+
+        else:
+            logging.debug("CSV Output Disabled")
+            self.server.enable_csv(False)
+
+    def on_closing(self):
+        if self.server.is_running():
+            self.server.stop()
+            self.update_status('Stopping')
+        self.showing = False
+
+        self.store_config({
+            'csv_file': self.csv_entry.get(),
+            'host': self.host_entry.get(),
+            'port': self.port_entry.get()
+            })
+
+        self.root.destroy()
+
+    def browse(self):
+        file_path = filedialog.askopenfilename(title="Select CSV File", filetypes=[("CSV files", "*.csv")])
+        self.csv_entry.delete(0, tk.END)
+        self.csv_entry.insert(0, file_path)
+        self.server.set_csv_file(file_path)
+
+    def clear_csv(self):
+        logging.debug("Clear CSV button pressed")
+        self.server.clear_csv()
+
+    async def run_tk(self):
+        try:
+            await self.show();
+        finally:
+            self.event_loop.close()
+
+    def run(self):
+        asyncio.run(self.run_tk())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Websocket Server to directly control the GPIO output of a microcontroller")
@@ -145,17 +354,44 @@ if __name__ == "__main__":
     parser.add_argument("--host", nargs="?", help="host for websocket (default: 127.0.0.1)")
     parser.add_argument("-p", "--port", type=int, nargs="?", help="port for websocket (default: 22300)")
     parser.add_argument("-c", "--csv", nargs="?", help="Optional CSV file for dumping GPIO output values each frame")
+    parser.add_argument("-d", "--debug", nargs="?", help="Enable Logging")
     
     args = parser.parse_args()
 
-    if args.csv is not None:
-        write_csv = True
-        csv_file_name = args.csv
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
-    if args.host is not None:
-        serve_host = args.host
+    serve_host = "127.0.0.1"
+    serve_port = 22300
+    csv_file_name = None
 
-    if args.port is not None:
-        serve_port = args.port   
+    if args.csv is not None or args.host is not None or args.port is not None:
+        if args.csv is not None:
+            csv_file_name = args.csv
+    
+        if args.host is not None:
+            serve_host = args.host
+    
+        if args.port is not None:
+            serve_port = args.port   
 
-    asyncio.run(main())
+        server = WebSocketServer()
+        server.set_host(serve_host)
+        server.set_port(serve_port)
+
+        if args.csv is not None:
+            server.enable_csv(True)
+            server.set_csv_file(args.csv)
+    
+        asyncio.run(server.main())
+    else:
+        import tkinter as tk
+        from tkinter import filedialog
+        import appdirs
+        import os
+        import re
+
+        gui = WebSocketGUI()
+        gui.run()
